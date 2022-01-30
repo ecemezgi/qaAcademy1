813182081459e3957994fa573ce7e30b1ac6a827
ecemezgi <ecemezgievruk@gmail.com>
2022-01-30 22:23:07 +0300

    ok

---
+++ src/test/java/services/sample4Test.java
@@ -0,0 +1,23 @@
+package services;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class sample4Test {
+
+    @Test
+    public void getIventory(){
+        String baseUrl="https://petstore.swagger.io/v2/store/inventory";
+
+        RequestSpecification requestSpecification = RestAssured.given()
+                .header("Study","Test")
+                .log().all();
+
+        Response response = requestSpecification.get(baseUrl);
+        Assert.assertEquals(response.getStatusCode(),200);
+
+    }
+}
